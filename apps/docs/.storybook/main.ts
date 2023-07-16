f539f3b64997d41faf5743f45a7e5589701d8426
NakhunSong <nak4627@gmail.com>
2023-07-17 01:15:57 +0900

    feat: PQ-6

---
+++ apps/docs/.storybook/main.ts
@@ -0,0 +1,27 @@
+const path = require("path");
+const { mergeConfig } = require("vite");
+
+module.exports = {
+  stories: ["../stories/**/*.stories.mdx", "../stories/**/*.stories.tsx"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  framework: "@storybook/react",
+  core: {
+    builder: "@storybook/builder-vite",
+  },
+  async viteFinal(config, { configType }) {
+    // customize the Vite config here
+    return mergeConfig(config, {
+      resolve: {
+        alias: [
+          {
+            find: "@qui/react",
+            replacement: path.resolve(
+              __dirname,
+              "../../../packages/qui-react/"
+            ),
+          },
+        ],
+      },
+    });
+  },
+};
---
+++ apps/docs/stories/Button.stories.tsx
@@ -0,0 +1,33 @@
+import { Button } from "@qui/react/src";
+import { ArgTypes, Meta, Story } from "@storybook/react";
+import * as React from "react";
+import { ComponentProps } from "react";
+
+type MyArgTypes = Partial<
+  Record<keyof ComponentProps<typeof Button>, ArgTypes[string]>
+>;
+const argTypesSetting: MyArgTypes = {
+  children: {
+    description: "버튼 텍스트 또는 리액트 컴포넌트",
+  },
+  onClick: {
+    description: "버튼 클릭 이벤트 핸들러",
+  },
+};
+
+export default {
+  title: "Base/Button",
+  component: Button,
+  argTypes: argTypesSetting,
+  parameters: { actions: { argTypesRegex: "^on.*" } },
+} as Meta<typeof Button>;
+
+const Template: Story<ComponentProps<typeof Button>> = (props) => {
+  return <Button {...props}></Button>;
+};
+
+export const Default = Template.bind({});
+Default.args = {
+  children: "텍스트",
+  onClick: () => console.log("클릭"),
+};
