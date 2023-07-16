95eb1b1fc93b4967a6bf479bdd5b45f8a15d7186
nakhun-quantit <nakhun.song@quantit.io>
2023-07-17 03:41:21 +0900

    feat: PQ-7

--- apps/docs/stories/Button.stories.tsx
+++ apps/docs/stories/Button.stories.tsx
@@ -1,25 +1,25 @@
-import { Button } from "@qui/react/src";
-import { ArgTypes, Meta, Story } from "@storybook/react";
-import * as React from "react";
-import { ComponentProps } from "react";
+import { Button } from '@quantit/react/src';
+import { ArgTypes, Meta, Story } from '@storybook/react';
+import * as React from 'react';
+import { ComponentProps } from 'react';
 
 type MyArgTypes = Partial<
   Record<keyof ComponentProps<typeof Button>, ArgTypes[string]>
 >;
 const argTypesSetting: MyArgTypes = {
   children: {
-    description: "버튼 텍스트 또는 리액트 컴포넌트",
+    description: '버튼 텍스트 또는 리액트 컴포넌트',
   },
   onClick: {
-    description: "버튼 클릭 이벤트 핸들러",
+    description: '버튼 클릭 이벤트 핸들러',
   },
 };
 
 export default {
-  title: "Base/Button",
+  title: 'Base/Button',
   component: Button,
   argTypes: argTypesSetting,
-  parameters: { actions: { argTypesRegex: "^on.*" } },
+  parameters: { actions: { argTypesRegex: '^on.*' } },
 } as Meta<typeof Button>;
 
 const Template: Story<ComponentProps<typeof Button>> = (props) => {
@@ -28,6 +28,6 @@ const Template: Story<ComponentProps<typeof Button>> = (props) => {
 
 export const Default = Template.bind({});
 Default.args = {
-  children: "텍스트",
-  onClick: () => console.log("클릭"),
+  children: '텍스트',
+  onClick: () => console.log('클릭'),
 };
