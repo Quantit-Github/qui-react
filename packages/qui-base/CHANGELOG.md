0ea5ebdbfc5ce2b80597cc47ef313f3fe7eb2fd9
nakhun-quantit <nakhun.song@quantit.io>
2025-01-17 14:42:31 +0900

    NO-ISSUE 아이콘 추가
    - pulse 아이콘 추가

--- packages/qui-base/CHANGELOG.md
+++ packages/qui-base/CHANGELOG.md
@@ -1,5 +1,11 @@
 # @quantit/qui-react
 
+## 0.3.67
+
+### Patch Changes
+
+- feat: add icon
+
 ## 0.3.66
 
 ### Patch Changes
--- packages/qui-base/lib/components/Icon/type.ts
+++ packages/qui-base/lib/components/Icon/type.ts
@@ -51,6 +51,7 @@ const ICON_TYPES = [
   'plus',
   'portfolio',
   'profile',
+  'pulse',
   'pythonProject',
   'quick',
   'rebalancing',
